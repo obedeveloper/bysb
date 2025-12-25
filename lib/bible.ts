59f96ec2dce56f0f02d04821279493c542e51ae4
NIYOMUGISHA Obed <obed6@duck.com>
2025-12-25 13:00:45 +0200

    Fix: make chapter key an array, even for books with single chapter

--- lib/bible.ts
+++ lib/bible.ts
@@ -1,12 +1,24 @@
-import { XMLParser } from "fast-xml-parser";
-import { Bible } from "./types.ts";
+import { XMLParser } from 'fast-xml-parser';
+import { Bible } from './types.ts';
 
-const xmlText = await Deno.readTextFile("bysb.xml");
+const xmlText = await Deno.readTextFile('bysb.xml');
 const parser = new XMLParser({
   ignoreAttributes: false,
-  textNodeName: "content",
-  attributeNamePrefix: "",
+  textNodeName: 'content',
+  attributeNamePrefix: '',
 });
 
-export default (parser.parse(xmlText) as { XMLBIBLE: Bible }).XMLBIBLE
-  .BIBLEBOOK;
+export default (
+  parser.parse(xmlText) as { XMLBIBLE: Bible }
+).XMLBIBLE.BIBLEBOOK.map((b) => {
+  const chapter = b.CHAPTER;
+
+  if (!Array.isArray(chapter)) {
+    return {
+      ...b,
+      CHAPTER: [chapter],
+    };
+  }
+
+  return b;
+});
